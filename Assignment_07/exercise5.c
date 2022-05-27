3823557b2e21c2903ba9b6f5e09853e539d19cff
Ponhrith <ponhrith.nhep@gmail.com>
2022-05-27 22:34:27 +0700

    testomg

--- Assignment_07/exercise5.c
+++ Assignment_07/exercise5.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
 int main(){
+    float number;
+    printf("Enter the number:");
+    scanf("%f", &number);
+    if (number < 10) { 
+        printf("Too low");
+    }  else if (number > 10){
+        printf("Correct");
+    }  else if (number < 20){
+        printf("Correct");
+    }  else {
+        printf("Too high");
+    }  
     
+return 0;
+
 }
