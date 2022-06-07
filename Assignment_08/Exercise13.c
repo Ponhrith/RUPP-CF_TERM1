cc1e5b37752711db1801d07156092871a47f0fad
Ponhrith <ponhrith.nhep@gmail.com>
2022-06-07 22:02:21 +0700

    comment added

--- Assignment_08/Exercise13.c
+++ Assignment_08/Exercise13.c
@@ -2,10 +2,10 @@
 int main(){
     int num;
     do{
-        printf("Enter a number:");
+        printf("Enter a number:"); //Ask users to input a number
         scanf("%d", &num);
-    }while (num<=5);
-    if (num>5){
+    }while (num<=5); //keep asking until the value over 5
+    if (num>5){ 
         printf("Your final number is %d", num);
     }
 }
--- Assignment_08/Exercise15.c
+++ Assignment_08/Exercise15.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
 int main()
-{
+{   
+    //Create variable
     int computer_num = 50;
     int input,count=0;
     while (count>-1)
     {
-        printf("Enter the computer_num:");
+        printf("Enter the computer_num:"); //Ask users to enter a number
         scanf("%d",&input);
         count ++;
-        if (input<computer_num)
+        if (input<computer_num) //show too low if the input<50
         {
             printf("To low\n");
         }
         else if (input>computer_num)
         {
-            printf("To high\n");
+            printf("To high\n"); //show too high if the input>50
         }
         else
         {
-            printf("Well done, you took %d attemps",count);
+            printf("Well done, you took %d attemps",count); //print well done if input = 50
             break;
         }
         
