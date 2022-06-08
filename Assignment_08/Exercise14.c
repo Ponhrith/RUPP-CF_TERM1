c70f95834b88fa81be43ceff82cb24ce86608761
Ponhrith <ponhrith.nhep@gmail.com>
2022-06-08 07:44:55 +0700

    done

--- Assignment_08/Exercise14.c
+++ Assignment_08/Exercise14.c
@@ -1,56 +1,33 @@
 #include <stdio.h>
-//#include <string.h>
 int main(){
-    int num;
+    int num, num1;
+    char choice;
+
     printf("Enter a number:");
     scanf("%d", &num);
-    int num1;
     printf("Enter another number:");
     scanf("%d", &num1);
-    int num2;
-    num2 = num + num1;
-    printf("Add the two number:%d\n", num2);
-
-    char choice;
-    // printf("Do you want to add a number?");
-    printf("Click y to continue! ");
+    num1 += num; //num1 = num1 + num
+    
+    printf("Click y to continue!");
     scanf(" %c", &choice);
-    // char continue_program[0];
-    // continue_program[0] = 'y';
-    //int value = strcmp(choice,continue_program);
-    int num3;
-    int sum;
-    if (choice == 'y'){
-        do{
-            printf("Input another number ");
-            scanf("%d", &num3);
-            sum = num3 + num2;
-            printf("%d\n", sum);
-            printf("Click y to continue! ");
-            scanf(" %c", &choice);
-           
-        }while(choice=='y');
-        
+    if  (choice != 'y'){
+        printf("Display the total %d", num1);
     }
-    // while(choice == 'y')
-    // {
-    //     printf("Input another number ");
-    //     scanf("%d", num3);
-    //     sum = num3 + num2;
-    //     printf("Click y to continue! ");
-    //     scanf("%s", choice);
-    //     if(choice != 'y'){
-    //         printf("Final Total %d",sum);
-    //         break;
-    //     }
-    // }
     
-    // do{
-    //     printf("Enter a number:");
-    //     scanf("%d", &num3);
-    //     sum = num2 + num3;
-    // }while (choice == 'y');
-    
+    while (choice == 'y'){
+    printf("Enter a new number:");
+    scanf("%d", &num);
+
+    num1 += num;
+
+    printf("Click y to continue!");
+    scanf(" %c", &choice);
+
+    if  (choice != 'y'){
+        printf("Display the total %d", num1);
+        break;
+    }
+    }
+    return 0;
 }
-
-
